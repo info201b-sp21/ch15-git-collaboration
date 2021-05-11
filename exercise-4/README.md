6287be5a0bb29e6b1ebb91a6b3a97296f97470dc
Ott Toomet <otoomet@uw.edu>
2021-05-10 17:17:33 -0700

    ex4: code cleaner

--- exercise-4/README.md
+++ exercise-4/README.md
@@ -1,14 +1,14 @@
 # Exercise-4
 
 In this exercise, you'll edit the same file with a partner and resolve
-the merge conflicts that almost always will arise.  Your taks are
+the merge conflicts that almost always will arise.  Your tasks are
 
 1. fork and clone this repository,
 2. follow the instructions in `exercise.R` file,
-3. then `add` and `commit` your changes so that you can `push` them
-back up to Github.
+3. then `commit` your changes so that you can `push` them
+   back up to Github.
 4. finally, ensure that both of you end up with the same conflict-free
-   version of the repo.
+   version of the repo, and the same version is visible on Github too.
 
 Learning how to successfully collaborate in a team to pull and merge
 one another's changes will make your life a lot easier than juggling
@@ -17,4 +17,4 @@ different files. With this exercise you should practice using both
 should avoid communicating your changes to the code in order to
 stimulate a "real-world" scenario.
 
-Note that there is no completed branch for this exercise.
+Note that there is no solutions for this exercise.
--- exercise-4/exercise.r
+++ exercise-4/exercise.r
@@ -2,11 +2,9 @@
 
 ## Your task is to fix problems in these three code snippets, and write a simple function
 
-## 1. Install the nycflights13 package (unless you have done it earlier) and load it.
-# install.packages("nycflights13")
-## Require the dplyr package.
-library(nycflights)
+## Load the dplyr package and flights data (the same flights data we used in class)
 library("dplyr")
+flights <- read.delim("nycflights13.csv.bz2")
 
 ## 2. In which month was the average departure delay the greatest?
 dep.delay.by.month <- flights %>% 
@@ -19,8 +17,12 @@ arr.delay.by.month <- flights %>%
   summarise(delay == mean(dep_delay %>% 
   select(arrange(delay))
   
-## 4. Each team member should write a function that takes in a vector of movie titles and assigns a random rating
-## to each movie title (Hint: use the runif function). Push the code and solve the merge conflics.
+## 4. Each team member should write a function that takes in a vector of movie titles and
+## assigns a random rating
+## to each movie title.
+## You can use e.g. `runif(5)` to create 5 random numbers.
+## Push the code and solve the merge conflics.
 
 ## 5. Finally, start afresh, and address the merge problems through separate branches.
-  ##  i.e. you will create a separate branch for development.  When done, you merge your side branch into the master.
+##  i.e. you will create a separate branch for development.  When done, you merge your side
+##  branch into main.
